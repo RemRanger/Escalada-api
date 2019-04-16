ce1520e7f743b9080fef902e8a95378e44d11b62
RemRanger <rxb@ziggo.nl>
2019-04-16 16:30:38 +0200

    Register.

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+session_start();
+include_once 'dbconnect.php';
+
+$userName = mysqli_real_escape_string($conn, $_POST['userName']);
+$firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
+$lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
+$gender = mysqli_real_escape_string($conn, $_POST['gender']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = md5(mysqli_real_escape_string($conn, $_POST['password']));
+ 
+if(mysqli_query($conn, "INSERT INTO User(Username, FirstName, LastName, Gender, Email, Password) VALUES('$userName', '$firstName', '$lastName', '$gender', '$email', '$password')"))
+{
+	$sql = "SELECT LAST_INSERT_ID() id";
+
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) 
+	{
+		$row = $result->fetch_assoc();
+
+		echo json_encode($row, JSON_NUMERIC_CHECK);
+	} 
+}
