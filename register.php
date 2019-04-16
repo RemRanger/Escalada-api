beede6e926e7bf1966353a69a4498005db6b801a
RemRanger <rxb@ziggo.nl>
2019-04-16 16:58:32 +0200

    Register checks for duplicate user name.

--- register.php
+++ register.php
@@ -11,17 +11,25 @@ $lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
 $gender = mysqli_real_escape_string($conn, $_POST['gender']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
- 
-if(mysqli_query($conn, "INSERT INTO User(Username, FirstName, LastName, Gender, Email, Password) VALUES('$userName', '$firstName', '$lastName', '$gender', '$email', '$password')"))
-{
-	$sql = "SELECT LAST_INSERT_ID() id";
 
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) 
+$result = $conn->query("select count(*) existingCount from User where Username='$userName'");
+$row = $result->fetch_assoc();
+$existingCount = $row["existingCount"];
+if ($existingCount == 0) 
+{ 
+	if(mysqli_query($conn, "INSERT INTO User(Username, FirstName, LastName, Gender, Email, Password) VALUES('$userName', '$firstName', '$lastName', '$gender', '$email', '$password')"))
 	{
-		$row = $result->fetch_assoc();
+		$sql = "SELECT LAST_INSERT_ID() id";
 
-		echo json_encode($row, JSON_NUMERIC_CHECK);
-	} 
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) 
+		{
+			$row = $result->fetch_assoc();
+
+			echo json_encode($row, JSON_NUMERIC_CHECK);
+		} 
+	}
 }
+else
+	echo '{"id":-1}';
