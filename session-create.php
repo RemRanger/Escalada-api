ee6564495c43bc3c5ce3fc61e7265b10ceaab89e
RemRanger <rxb@ziggo.nl>
2019-04-18 15:42:27 +0200

    Session create.

--- session-create.php
+++ session-create.php
@@ -1,13 +1,17 @@
 <?php
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST");
 session_start();
 include_once 'dbconnect.php';
 
-$IdUser = $_SESSION["IdUser"];
-
 $IdLocation = mysqli_real_escape_string($conn, $_POST['locationId']);
 $Date = mysqli_real_escape_string($conn, $_POST['date']);
-$PartnerIds = $_POST['PartnerIds'];
+$PartnerIds = $_POST['partnerIds'];
 $Comment = mysqli_real_escape_string($conn, $_POST['comment']);
+
+$IdUser = $_POST["userId"];
 
 $success = True;
 if(!mysqli_query($conn, "INSERT INTO Session (IdLocation, Date) VALUES('$IdLocation', '$Date')"))
@@ -25,14 +29,14 @@ if ($success)
 	}
 }
 
-if ($success && !empty($PartnerIds))
-{
-	foreach ($PartnerIds as $IdPartner)
-	{
-		if(!mysqli_query($conn, "INSERT INTO SessionToUser (IdSession, IdUser) VALUES('$IdSession', '$IdPartner')"))
-		{
-			$success = False;
-			break;
-		}
-	}	
-}
+// if ($success && !empty($PartnerIds))
+// {
+	// foreach ($PartnerIds as $IdPartner)
+	// {
+		// if(!mysqli_query($conn, "INSERT INTO SessionToUser (IdSession, IdUser) VALUES('$IdSession', '$IdPartner')"))
+		// {
+			// $success = False;
+			// break;
+		// }
+	// }	
+// }
