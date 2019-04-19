9157e4a1ef8122a0b53a8cf549816983df80e953
RemRanger <rxb@ziggo.nl>
2019-04-19 11:38:51 +0200

    Session delete.

---
+++ session-delete.php
@@ -0,0 +1,19 @@
+<?php
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST");
+session_start();
+include_once 'dbconnect.php';
+
+
+$Id = mysqli_real_escape_string($conn, $_POST['id']);
+
+$success = True;
+if(!mysqli_query($conn, "DELETE FROM SessionToUser WHERE IdSession = '$Id'"))
+	$success = False;
+if(!mysqli_query($conn, "DELETE FROM Session WHERE Id = '$Id'"))
+	$success = False;
+
+if ($success)
+	echo '{"id":' . $Id . '}';
