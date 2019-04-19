242e5c1fd0f1636be1dc77ef013c406f8b27ad22
RemRanger <rxb@ziggo.nl>
2019-04-19 16:25:07 +0200

    Attempt edit/create.

--- attempt-read.php
+++ attempt-read.php
@@ -4,13 +4,14 @@ header("Access-Control-Allow-Origin: *");
 session_start();
 include_once 'dbconnect.php';
 
+$Id = $_GET["id"];
 $IdSession = $_GET["sessionId"];
 $IdUser = $_GET["userId"];
 
 $sql = "
 select Att.Id id, Att.Result result, Att.Percentage percentage, Att.Comment comment, 
 Rou.Id routeId, Rou.Color routeColor, Rou.Name routeName, Rou.Type routeType, Rou.Rating routeRating, Rou.Sublocation routeSublocation, Rou.DateUntil routeDateUntil, Rou.PictureFileName routePictureFileName,
-Loc.Name locationName,
+Loc.Id locationId, Loc.Name locationName,
 Ses.Id sessionId, Ses.Date sessionDate,
 Usr.Id userId, Usr.FirstName userFirstName, Usr.LastName userLastName
 from Attempt Att
@@ -18,12 +19,17 @@ join Route Rou on Rou.Id = Att.IdRoute
 join Session Ses on Ses.Id = Att.IdSession
 join Location Loc on Loc.Id = Ses.IdLocation 
 join User Usr on Usr.Id = Att.IdUser";
-if (isset($IdSession) && isset($IdUser))
-	$sql .= " where Att.IdSession = $IdSession and Att.IdUser = $IdUser";
-else if (isset($IdSession))
-	$sql .= " where Att.IdSession = $IdSession";
-else if (isset($IdUser))
-	$sql .= " where Att.IdUser = $IdUser";
+if (isset($Id))
+	$sql .= " where Att.Id = $Id";
+else
+{
+	if (isset($IdSession) && isset($IdUser))
+		$sql .= " where Att.IdSession = $IdSession and Att.IdUser = $IdUser";
+	else if (isset($IdSession))
+		$sql .= " where Att.IdSession = $IdSession";
+	else if (isset($IdUser))
+		$sql .= " where Att.IdUser = $IdUser";
+}
 
 $sql .= " order by Ses.Date desc, Ses.Id desc, Usr.Id, Att.Order, Att.Id";
 
--- session-read.php
+++ session-read.php
@@ -4,23 +4,38 @@ header("Access-Control-Allow-Origin: *");
 session_start();
 include_once 'dbconnect.php';
 
+$Id = $_GET["id"];
 $UserId = $_GET["userId"];
 
+$sql = "";
+if (isset($UserId))
+{
+	$sql = "
+	select 
+		Ses.Id id, SesToUsr.Comment comment, Ses.Date date, Loc.Id locationId, Loc.Name locationName, 
+		GROUP_CONCAT(Usr.Id SEPARATOR ',') partnerIdsAsString,
+		GROUP_CONCAT(DISTINCT CONCAT(Usr.FirstName, ' ', Usr.LastName) ORDER BY Usr.FirstName SEPARATOR ', ') partnerNames
+	from Session Ses
+	join SessionToUser SesToUsr on SesToUsr.IdSession = Ses.Id
+	join Location Loc on Loc.Id = Ses.IdLocation
+	left outer join SessionToUser SesToUsrWith on SesToUsrWith.IdSession = Ses.Id and SesToUsrWith.IdUser <> $UserId
+	left outer join User Usr on Usr.Id = SesToUsrWith.IdUser
+	where SesToUsr.IdUser = $UserId
+	group by Ses.Id, SesToUsr.Comment, Ses.Date, Loc.Name
+	order by Ses.Date desc, Ses.Id desc
+	";
+}
+elseif (isset($Id))
+{
+	$sql = "
+	select 
+		Ses.Id id, Ses.Date date, Loc.Id locationId, Loc.Name locationName
+	from Session Ses
+	join Location Loc on Loc.Id = Ses.IdLocation
+	where Ses.Id = $Id
+	";
+}
 
-$sql = "
-select 
-	Ses.Id id, SesToUsr.Comment comment, Ses.Date date, Loc.Id locationId, Loc.Name locationName, 
-	GROUP_CONCAT(Usr.Id SEPARATOR ',') partnerIdsAsString,
-	GROUP_CONCAT(DISTINCT CONCAT(Usr.FirstName, ' ', Usr.LastName) ORDER BY Usr.FirstName SEPARATOR ', ') partnerNames
-from Session Ses
-join SessionToUser SesToUsr on SesToUsr.IdSession = Ses.Id
-join Location Loc on Loc.Id = Ses.IdLocation
-left outer join SessionToUser SesToUsrWith on SesToUsrWith.IdSession = Ses.Id and SesToUsrWith.IdUser <> $UserId
-left outer join User Usr on Usr.Id = SesToUsrWith.IdUser
-where SesToUsr.IdUser = $UserId
-group by Ses.Id, SesToUsr.Comment, Ses.Date, Loc.Name
-order by Ses.Date desc, Ses.Id desc
-";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) 
